70dc45bc4d7e51af521e40bc53bb8b59c747c5d7
Derek Ding <derek.ding727@gmail.com>
2023-02-24 17:10:22 +0800

    oop-formatted the main file, next step is to add dall.e function to the askgpt class

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
 import os
 import openai
-from flask import Flask, redirect, render_template, request, url_for, app
-
 import tkinter as tk
+# askgpt class
 import askgpt as asklegpt
+
+# read the apikey from local dir
 with open(r"D:\aAPIkeys\openaiprivate.txt", 'r') as f:
     openai.api_key = f.readline()
     f.close()
-# print(openai.api_key)
-# openai.api_key = ""
-openai.Model.list()
+
 # Set theme for the GUI
 BG_GRAY = "#ABB2B9"
 BG_COLOR = "#0c0c0d"
@@ -17,70 +16,71 @@ TEXT_COLOR = "#28fa19"
 FONT = "Helvetica 14"
 FONT_BOLD = "Helvetica 13 bold"
 
-# create the window
-window = tk.Tk()
 
-# set the window size
-#window.geometry('900x500')
+# OOP Style:
+class ChatBotHomePage:
 
-# set the window title
-window.title("ChatGPT GUI")
+    def __init__(self, master):
+        """
+        The home window
+        """
+        self.master = master
+        # add window title:
+        self.master.title("ChatGPT GUI")
 
-# add a label
-lable1 = tk.Label(window, bg=BG_COLOR, fg=TEXT_COLOR, text="Welcome to ChatGPT", font=FONT_BOLD, pady=10, width=20,
-                  height=1).grid(row=0, columnspan=2)
+        # add label
+        tk.Label(master, bg=BG_COLOR, fg=TEXT_COLOR, text="Welcome to ChatGPT", font=FONT_BOLD, pady=10,
+                 width=20, height=1).grid(row=0, columnspan=2)
 
-# add the main interfacing text box
-txt = tk.Text(window, bg=BG_COLOR, fg=TEXT_COLOR, font=FONT, width=130, height= 32)
-txt.grid(row=1, column=0, columnspan=2)
-txt.insert('1.0', 'Aks me anything:')
+        # define the main textbox:
+        self.master.txt = tk.Text(master, bg=BG_COLOR, fg=TEXT_COLOR, font=FONT, width=130, height=32)
+        self.master.txt.grid(row=1, column=0, columnspan=2)
+        self.master.txt.insert('1.0', 'Aks me anything:')
 
-# for scrolling (auto)
-scrollbar = tk.Scrollbar(txt)
-scrollbar.place(relheight=1, relx=1.01)
+        # add scrollbars
+        self.master.scrollbar = tk.Scrollbar(self.master.txt)
+        self.master.scrollbar.place(relheight=1, relx=1.01)
 
-# entry text-box
-entry = tk.Entry(window, bg=BG_COLOR, fg=TEXT_COLOR, font=FONT, width=100)
-entry.configure(bg=BG_COLOR, insertbackground=TEXT_COLOR, insertwidth=5)
-# this function to adjust cursor location is not effective,entry.icursor(entry.index(tk.END)+555)
-# height=215, width=200)
-entry.grid(row=2, columnspan=2, padx=10, pady=20, ipady=3)
+        # add a standard entry box, configure its appearance and location
+        self.master.entry = tk.Entry(master, bg=BG_COLOR, fg=TEXT_COLOR, font=FONT, width=100)
+        self.master.entry.configure(bg=BG_COLOR, insertbackground=TEXT_COLOR, insertwidth=5)
+        self.master.entry.grid(row=2, columnspan=2, padx=10, pady=20, ipady=3)
 
-# add event:
-def clicked(event):
-    # At the start of each conversation
+        # Create the button, configure its appearance and location
+        self.master.sendbtn = tk.Button(master, text="Send", font=FONT_BOLD, bg=BG_GRAY)
+        self.master.sendbtn.bind("<Button-1>", self.clicked)
+        self.master.sendbtn.grid(row=3, column=0)
 
-    send = "Q -> " + entry.get()
-    txt.insert(tk.END, "\n" + send)
-    user = entry.get().lower()
-    print(user)
-    txt.insert(tk.END, "\n" + "A -> " + asklegpt.askGPT(user)[2:] + "\n")
-    entry.delete(0, tk.END)
+        # bind the send button with ctrl + enter:
+        self.master.bind('<Control-Return>', self.clicked)
 
-    txt.see(tk.END)
-    # end chat
-    entry.delete(0, tk.END)
+        # bind a shortcut quit key:
+        def quit(event):
+            print("you pressed control-q")
+            self.master.quit()
 
-# Create the button
-sendbtn = tk.Button(window, text="Send", font=FONT_BOLD, bg=BG_GRAY)
-sendbtn.bind("<Button-1>", clicked)
+        # bind the quit function with control + q:
+        self.master.bind('<Control-q>', quit)
 
-# place the send button
-sendbtn.grid(row=3, column=0)
+    def clicked(self, event):
+        """
+        This function defines the cliked event, the api request is included
 
-# bind the send button with ctrl + enter:
-window.bind('<Control-Return>', clicked)
+        Returns: no return for this function
+        """
+        send = "Q -> " + self.master.entry.get()
+        self.master.txt.insert(tk.END, "\n" + send)
+        user = self.master.entry.get().lower()
+        ### check if null input ###
+        print(user)
+        self.master.txt.insert(tk.END, "\n" + "A -> " + asklegpt.askGPT(user)[2:] + "\n")
+        self.master.entry.delete(0, tk.END)
 
-# bind a shortcut quit key:
-def quit(event):
-    print("you pressed control-q")
-    window.quit()
+        self.master.txt.see(tk.END)
+        # end chat
+        self.master.entry.delete(0, tk.END)
 
-# bind the send button with with ctrl + enter:
-window.bind('<Control-q>', quit)
-
-# add another button, clear screen:
-
-
-# run the window
-window.mainloop()
+# start running the home window
+root = tk.Tk()
+app = ChatBotHomePage(root)
+root.mainloop()
--- webprac.py
+++ webprac.py
@@ -2,6 +2,9 @@
 # the goal for this project is to build a small web app to interact with openai
 # models via api.
 # also, the model choice and model parameters are customizable with explanations.
-import flask
+# import flask
+from flask import Flask, redirect, render_template, request, url_for, app
 
-
+# print(openai.api_key)
+# openai.api_key = ""
+# openai.Model.list()
