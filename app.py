dd4a5857ac2aea877d78d56c7abb1c2ff5c53b2c
Derek Ding <derek.ding727@gmail.com>
2023-02-27 16:50:19 +0800

    added mad mode

--- app.py
+++ app.py
@@ -9,6 +9,11 @@ with open(r"D:\aAPIkeys\openaiprivate.txt", 'r') as f:
     openai.api_key = f.readline()
     f.close()
 
+# read the test line:
+with open(r"D:\aAPIkeys\madgpt.txt", 'r') as f:
+    madmode = f.readline()
+    f.close()
+
 # Set theme for the GUI
 BG_GRAY = "#ABB2B9"
 BG_COLOR = "#0c0c0d"
@@ -54,13 +59,21 @@ class ChatBotHomePage:
         # bind the send button with ctrl + enter:
         self.master.bind('<Control-Return>', self.clicked)
 
+        # mad mode, Create the button, configure its appearance and location
+        self.master.sendmadbtn = tk.Button(master, text="MadSend", font=FONT_BOLD, bg=BG_GRAY)
+        # bind left mouse click
+        self.master.sendmadbtn.bind("<Button-1>", self.madclicked)
+        self.master.sendmadbtn.grid(row=3, column=1)
+
+
         # bind a shortcut quit key:
-        def quit(event):
+        def quitchat(event):
             print("you pressed control-q")
+            # tkinter method quit()
             self.master.quit()
 
         # bind the quit function with control + q:
-        self.master.bind('<Control-q>', quit)
+        self.master.bind('<Control-q>', quitchat)
 
     def clicked(self, event):
         """
@@ -71,14 +84,55 @@ class ChatBotHomePage:
         send = "Q -> " + self.master.entry.get()
         self.master.txt.insert(tk.END, "\n" + send)
         user = self.master.entry.get().lower()
-        ### check if null input ###
-        print(user)
-        self.master.txt.insert(tk.END, "\n" + "A -> " + asklegpt.askGPT(user)[2:] + "\n")
-        self.master.entry.delete(0, tk.END)
 
+        # store asked question in another dir:
+        gptanswertext = asklegpt.askGPT(user)[2:]
+
+        # insert the reply to the textbox
+        self.master.txt.insert(tk.END, "\n" + "A -> " + gptanswertext + "\n")
+
+        # store our q and a locally
+        with open(r"D:\gptQandA.txt", 'a') as f1:
+            # write the question and answer into our txt file
+            f1.write("\n" + "Q: " + user + "\n")
+            f1.write("A: " + gptanswertext + "\n")
+            f1.close()
+
+        # clear entry:
+        self.master.entry.delete(0, tk.END)
         self.master.txt.see(tk.END)
         # end chat
         self.master.entry.delete(0, tk.END)
+
+    def madclicked(self, event):
+        """
+        This function defines the cliked event, the api request is included
+
+        Returns: no return for this function
+        """
+        send = "Q -> " + self.master.entry.get()
+        self.master.txt.insert(tk.END, "\n" + send)
+        user = self.master.entry.get().lower()
+
+        # store asked question in another dir:
+        madgptanswertext = asklegpt.askGPT(madmode + user)[2:]
+
+        # insert the reply to the textbox
+        self.master.txt.insert(tk.END, "\n" + "A -> " + madgptanswertext + "\n")
+
+        # store our q and a locally
+        with open(r"D:\gptQandA.txt", 'a') as f1:
+            # write the question and answer into our txt file
+            f1.write("\n" + "Q: " + user + "\n")
+            f1.write("A: " + gptanswertext + "\n")
+            f1.close()
+
+        # clear entry:
+        self.master.entry.delete(0, tk.END)
+        self.master.txt.see(tk.END)
+        # end chat
+        self.master.entry.delete(0, tk.END)
+
 
 # start running the home window
 root = tk.Tk()
