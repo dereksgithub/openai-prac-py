b59abd9bdbbfe2e50de3af0b78188a3a0b556db5
Derek Ding <derek.ding727@gmail.com>
2023-03-07 17:06:45 +0800

    made minor changes, started developing chat function using the  latest v 27 of openai package, and gpt3.5 model.

--- askgpt.py
+++ askgpt.py
@@ -3,11 +3,20 @@ import webbrowser
 
 
 def askGPT(gpt_prompt):
+    """
+
+    Args:
+        gpt_prompt: User questions, text prompts etc.
+
+    Returns: the response from gpt - the default model is davinci-003
+
+    """
+    tokenlimit = 1200
     response = openai.Completion.create(
         model="text-davinci-003",
         prompt=gpt_prompt,
         temperature=0.7,
-        max_tokens=1200,
+        max_tokens=tokenlimit,
         top_p=1,
         frequency_penalty=0.0,
         presence_penalty=0.0
@@ -26,5 +35,30 @@ def askdalle(prompt):
     dalleurl = response['data'][0]['url']
     webbrowser.open(dalleurl, new=0, autoraise=True)
 
-    storetopath = r"D:\OneDrive\DALLEImageStorage"
+    # storetopath = r"D:\OneDrive\DALLEImageStorage"
     return (response['data'][0]['url'])
+
+
+def chatwithGPT(gpt_prompt):
+    """
+
+    This is a new development for chatting with the latest model, 3.5
+
+    Args:
+        gpt_prompt: User questions, text prompts etc.
+
+    Returns: the response from gpt - the default model is davinci-003
+
+    """
+    tokenlimit = 1200
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=gpt_prompt,
+        temperature=0.7,
+        max_tokens=tokenlimit,
+        top_p=1,
+        frequency_penalty=0.0,
+        presence_penalty=0.0
+        # stop=["\n"]
+    )
+    return (response['choices'][0]['text'])
